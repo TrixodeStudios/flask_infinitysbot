b49bc3c51fcb521a7e5096f1fe633042f8f689ac
TrixodeStudios <trixodestudios@gmail.com>
2024-04-04 10:07:10 -0500

    Some changes for api

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.route('/', methods=['GET'])
+def homepage():
+    return "Homepage"
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello"
+
+@app.route('/postdata', methods=['POST'])
+def postdata():
+    data = request.json
+    print("Received data:", data)
+    return jsonify({"success": True, "msg": "Data received"}), 200
 
 if __name__ == '__main__':
   app.run(port=5000)
+
+
+
